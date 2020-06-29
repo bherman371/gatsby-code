3a1b32a6c16e842852a27b6d6e08bafb6b0e3455
Benjamin Herman <bherman371@gmail.com>
2020-06-29 13:00:30 -0400

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,55 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const getSiteMetadata = graphql`
+{
+  site {
+    siteMetadata {
+      title
+      author
+      createdAt
+    }
+  }
+}`
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default function Layout({ children }) {
+  return (
+    <StaticQuery
+      query={getSiteMetadata}
+      render={data => (
+        <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0 1.0875rem 1.45rem`,
+            paddingTop: 0
+          }}
+        >
+          {children}
+          <footer>
+            Built by {data.site.siteMetadata.author}, Created{' © '} 
+            {data.site.siteMetadata.createdAt}
+          </footer>
+        </div>
+      </>
+      )}
+    />
+  )
+}
