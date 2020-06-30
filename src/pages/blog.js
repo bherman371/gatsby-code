0be368a03f1ee0971b1ce6cd5439a6a0a2c0ad0e
Benjamin Herman <bherman371@gmail.com>
2020-06-30 09:41:28 -0400

    Added blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+
+export default () => (
+    <Layout>
+        
+    </Layout>
+)
